0081d88aabbd6e43503dfc294b9891403dd5c3c5
dev-moo <mail4andrewm@gmail.com>
2016-04-05 15:20:06 +0800

    update

--- network-check.py
+++ network-check.py
@@ -1,41 +1,102 @@
 import os
-import time
+from time import sleep
 
-router = "192.168.1.1"
-lan = "eth0"
-wlan = "wlan0"
 
-iplan = "192.168.1.2"
-ipwlan = "192.168.1.3"
 
-def lanUp (hostname):
-	response = os.system("ping -I " + iplan + " -c 1 " + hostname)
-	return response
+def log_event(e):
+
+	tstamp = strftime("%Y%m%d%H%M%S")
 	
-def wlanUp (hostname):
-        response = os.system("ping -I " + ipwlan + " -c 1 " + hostname)
-        return response
+	print e
+	
+	f = open(os.path.dirname(os.path.abspath(__file__)) + '/log.txt', 'a')
+	f.write('"' + str(tstamp) + '","' + str(e) + '"\n')
+	f.close()
+
+
+def Test_Connection(rIP, sendIP):
+	return os.system("ping -I " + sendIP + " -c 1 " + rIP)
+
+	
+def Restore_Connection(rIP, ip, name):
+	
+	log_event("Attempting restore 1 on interface " + name)
+	os.system("sudo ifup " + name)
+
+	if Test_Connection(rIP, ip) == 0:
+		log_event("Restore successful on interface " + name)
+		return True	
+	else:
+		log_event("Restore 1 failed on interface " + name)
+		
+		
+	log_event("Attempting restore 2 on interface " + name)
+	
+	os.system("sudo ifdown " + name)
+	sleep(5)
+	os.system("sudo ifup " + name)
+
+	if Test_Connection(rIP, ip) == 0:
+		log_event("Restore successful on interface " + name)
+		return True			
+	else:
+		log_event("Restore 2 failed on interface " + name)		
+		
+	
+	log_event("Attempting restore 3 (reboot) of Pi " + name)
+	
+	os.system("sudo reboot")
+		
+		
+	return False
+	
+		
+def Check_Interface(rIP, ip, name):
+	
+	if Test_Connection(rIP, ip) == 0:
+		return True
+	
+	sleep(5)
+
+	if Test_Connection(rIP, ip) == 0:
+		return True	
+		
+	log_event("Ping failed on interface " + name)
+	return False
+		
+		
 	
 
-if lanUp (router) != 0:
-	os.system("sudo ifup " + lan)
 
-time.sleep(5)
+		
+if __name__ == "__main__":
+		
+	# Check config file exists	
 
-if lanUp (router) != 0:
-	os.system("sudo ifdown " + lan)
-	time.sleep(5)
-	os.system("sudo ifup " + lan)
+	dir = os.path.dirname(os.path.abspath(__file__))
+	filepath = dir + '/config.conf'
 
+	if not os.path.isfile(filepath):
+		print ("Error - Missing Config File: config.conf")
+		quit()
 
+	# Get config	
+	config = ConfigParser.ConfigParser()
+	config.read(filepath)
 
-if wlanUp (router) != 0:
-        os.system("sudo ifup " + wlan)
+	
+	routerIP = config.get('config', 'routerIP')	
+	ethIP = config.get('config', 'ethIP')
+	wlanIP = config.get('config', 'wlanIP')
+	ethName = config.get('config', 'lanname')
+	wlanName = config.get('config', 'wlanname')
+	
+	
+	if Check_Interface(routerIP, ethIP, ethName) == False:
+		Restore_Connection(routerIP, ethIP, ethName)
+		
+	if Check_Interface(routerIP, wlanIP, wlanName) == False:
+		Restore_Connection(routerIP, ethIP, ethName)	
+	
 
-time.sleep(5)
-
-if wlanUp (router) != 0:
-        os.system("sudo ifdown " + wlan)
-        time.sleep(5)
-        os.system("sudo ifup " + wlan)
 
